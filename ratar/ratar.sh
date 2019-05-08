c4e415c8ff9a0fbc074ea50c4d206bd39aa2fc11
Dominique Sydow <dominique.sydow@posteo.de>
2019-05-08 14:41:03 +0200

    Update input data path
    After removing 10>9 col mol2 pre-processing step (removed *_pp.mol2 files)

--- ratar/ratar.sh
+++ ratar/ratar.sh
@@ -24,19 +24,21 @@ output_dir="/home/dominique/Documents/projects/ratar-data/results"
 #python ratar.py -i ${data_dir}"/scpdb/full_20180807/*/site.mol2" -o ${output_dir}
 
 # Subset of scPDB
-#python ratar.py -i ${data_dir}"/scpdb/test_20180807/*/site.mol2" -o ${output_dir}"/scpdb/test_20180807/"
+python ratar.py -i ${data_dir}"/scpdb/test_20180807/*/site.mol2" -o ${output_dir}"/scpdb/test_20180807/"
 
 # Benchmarking dataset - FuzCav
-#python ratar.py -i ${data_dir}"/benchmarking/fuzcav/sim_dis_pairs/structures/*/site_CA_Met_pp.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
+#python ratar.py -i ${data_dir}"/benchmarking/fuzcav/sim_dis_pairs/structures/*/site_CA_Met.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
 
 # Benchmarking dataset - TOUGH-M1
-#python ratar.py -i ${data_dir}"/benchmarking/TOUGH-M1/structures/*/site_CA_Met_pp.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
+#python ratar.py -i ${data_dir}"/benchmarking/TOUGH-M1/structures/*/site_CA_Met.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
 
 # KLIFS EGFR
-#python ratar.py -i ${data_dir}"/klifs/egfr_20190506/structures/HUMAN/EGFR/*/pocket_pp.mol2" -o ${output_dir}"/klifs/egfr_20190509/HUMAN/EGRF/"
+#python ratar.py -i ${data_dir}"/klifs/egfr_20190506/structures/HUMAN/EGFR/*/pocket.mol2" -o ${output_dir}"/klifs/egfr_20190509/HUMAN/EGRF/"
 
 # KLIFS full
-#cd /home/dominique/Documents/klifs/klifs_20190506/structures/HUMAN
-#for i in *; do for j in $i"/"*; do echo $j"/pocket.mol2"; done; done
-#for i in *; do for j in $i"/"*; do less $j"/pocket.mol2" | awk '!($10="")' > $j"/pocket_pp.mol2"; done; done
-python ratar.py -i ${data_dir}"/klifs/klifs_20190506/structures/HUMAN/*/*/pocket_pp.mol2" -o ${output_dir}"/klifs/klifs_20190506/HUMAN/"
+#python ratar.py -i ${data_dir}"/klifs/klifs_20190506/structures/HUMAN/*/*/pocket.mol2" -o ${output_dir}"/klifs/klifs_20190506/HUMAN/"
+
+
+########################################################################################
+# Multiple pdb files
+########################################################################################
