ff58f46803c9e0ef53b280a9827e4761d21bc8ba
Dominique Sydow <dominique.sydow@posteo.de>
2019-06-04 16:59:51 +0200

    Add unit test docstrings and correct variable assignment
    Correct wrong assignment of DataFrame columns/rows to "mol_df_n_cols" and "mol_df_n_rows"

--- ratar/tests/test_ratar.py
+++ ratar/tests/test_ratar.py
@@ -22,20 +22,30 @@ def test_ratar_imported():
     ('scpdb_1a9p1.mol2', '1a9p_9DI_1_site', 463, 9),
     ('toughm1_1a0iA.pdb', 'data_toughm1_1a0iA', 160, 9)
 ])
-def test_molfileloader(path, mol_code, mol_df_n_cols, mol_df_n_rows):
+def test_molfileloader(filename, mol_code, mol_df_n_rows, mol_df_n_cols):
     """
     Test if molecule structure file is loaded correctly.
 
-    :return:
+    Parameters
+    ---------
+    filename : str
+        Name of molecule file.
+    mol_code : str
+        Name of molecule code.
+    mol_df_n_rows : int
+        Number of DataFrame rows.
+    mol_df_n_cols: int
+        Number of DataFrame columns.
+
     """
 
-    path = sys.path[0] / Path('ratar/tests/data') / Path(path)
+    path = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename
     mol_loader = MolFileLoader(path)
 
     assert len(mol_loader.pmols) == 1
     assert mol_loader.pmols[0].code == mol_code
-    assert mol_loader.pmols[0].df.shape[0] == mol_df_n_cols
-    assert mol_loader.pmols[0].df.shape[1] == mol_df_n_rows
+    assert mol_loader.pmols[0].df.shape[0] == mol_df_n_rows
+    assert mol_loader.pmols[0].df.shape[1] == mol_df_n_cols
 
 
 
