ccf9b9e99645f1476fc6f340c7702ef8c75c89dd
Dominique Sydow <dominique.sydow@posteo.de>
2019-06-05 10:04:23 +0200

    Add unit test: check binding site encoding against reference encoding

--- ratar/tests/test_ratar.py
+++ ratar/tests/test_ratar.py
@@ -7,6 +7,7 @@ import sys
 
 import pytest
 from pathlib import Path
+import pickle
 
 from ratar.auxiliary import MolFileLoader
 from ratar.encoding import BindingSite
@@ -48,24 +49,24 @@ def test_molfileloader(filename, mol_code, mol_df_n_rows, mol_df_n_cols):
     assert mol_loader.pmols[0].df.shape[1] == mol_df_n_rows
 
 
-@pytest.mark.parametrize('filename1, filename2', [
+@pytest.mark.parametrize('mol_file1, mol_file2', [
     ('AAK1_4wsq_altA_chainA.mol2', 'AAK1_4wsq_altA_chainB.mol2')
 ])
-def test_equal_bindingsites(filename1, filename2):
+def test_bindingsites_eq(mol_file1, mol_file2):
     """
     Test __eq__ functions for encoding classes.
 
     Parameters
     ----------
-    filename1 : str
-        Name of molecule file 1.
-    filename2 : str
-        Name of molecule file 2.
+    mol_file1 : str
+        Name of file containing the structure for molecule A.
+    mol_file2 : str
+        Name of file containing the structure for molecule B.
 
     """
 
-    path1 = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename1
-    path2 = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename2
+    path1 = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / mol_file1
+    path2 = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / mol_file2
 
     pmols1 = MolFileLoader(path1)
     pmols2 = MolFileLoader(path2)
@@ -77,3 +78,32 @@ def test_equal_bindingsites(filename1, filename2):
     assert (bs1 == bs2) is True
     assert (bs1 == bs3) is False
 
+
+@pytest.mark.parametrize('mol_file, encoding_file', [
+    ('AAK1_4wsq_altA_chainA.mol2', 'AAK1_4wsq_altA_chainA_encoded.p')
+])
+def test_bindingsites(mol_file, encoding_file):
+    """
+    Test if ratar-encoding of a molecule (based on its structural file) produces the same result
+    as the reference encoding for the same molecule.
+
+    Parameters
+    ----------
+    mol_file : str
+        Name of file containing the structure of molecule A.
+    encoding_file : str
+        Name of file containing ratar-encoding for molecule A.
+    """
+
+    # Encode binding site
+    path_mol = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / mol_file
+    pmols = MolFileLoader(path_mol)
+    bs = BindingSite(pmols.pmols[0])
+
+    # Load reference binding site encoding
+    path_encoding = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / encoding_file
+    with open(path_encoding, 'rb') as f:
+        bs_ref = pickle.load(f)
+
+    # Compare encoding with reference encoding
+    assert (bs == bs_ref) is True
