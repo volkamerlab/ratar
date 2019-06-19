9b1da1a51fcaba9357943b44ccb9a1fcd8fc369e
Unknown <dominique.sydow@posteo.de>
2019-06-19 21:00:29 +0200

    Add test_get_pca()
    Multi-line description of commit,
    feel free to be detailed.

--- ratar/tests/test_encoding.py
+++ ratar/tests/test_encoding.py
@@ -4,18 +4,19 @@ Unit and regression test for the ratar.encoding module of the ratar package.
 
 import sys
 
+from collections import Counter
 import pytest
 from pathlib import Path
 import pickle
 
-from ratar.auxiliary import MoleculeLoader
-from ratar.encoding import BindingSite
+from ratar.auxiliary import MoleculeLoader, load_pseudocenters
+from ratar.encoding import BindingSite, Representatives
 
 
 @pytest.mark.parametrize('mol_file1, mol_file2', [
     ('AAK1_4wsq_altA_chainA.mol2', 'AAK1_4wsq_altA_chainB.mol2')
 ])
-def test_bindingsites_eq(mol_file1, mol_file2):
+def ttest_bindingsites_eq(mol_file1, mol_file2):
     """
     Test __eq__ functions for encoding classes.
 
@@ -42,10 +43,41 @@ def test_bindingsites_eq(mol_file1, mol_file2):
     assert (bs1 == bs3) is False
 
 
+@pytest.mark.parametrize('filename, mol_n_atoms, pca_n_atoms', [
+    ('AAK1_4wsq_altA_chainA_reduced.mol2', 94, 34)
+])
+def test_get_pca(filename, mol_n_atoms, pca_n_atoms):
+    """
+    Test if pseudocenter atoms are correctly extracted from molecule.
+
+    Parameters
+    ----------
+    filename : str
+        Name of molecule file.
+    mol_n_atoms : int
+        Number of atoms in molecule.
+    pca_n_atoms : int
+        Number of pseudocenter atoms in molecule.
+    """
+
+    # Load molecule
+    path = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename
+    mol_loader = MoleculeLoader(path)
+    mol_loader.load_molecule()
+    molecule = mol_loader.pmols[0].df
+
+    # Set representatives
+    representatives = Representatives('')
+    representatives_pca = representatives._get_pca(molecule)
+
+    assert molecule.shape == (mol_n_atoms, 9)
+    assert representatives_pca.shape == (pca_n_atoms, 12)
+
+
 @pytest.mark.parametrize('mol_file, encoding_file', [
     ('AAK1_4wsq_altA_chainA.mol2', 'AAK1_4wsq_altA_chainA_encoded.p')
 ])
-def test_bindingsites(mol_file, encoding_file):
+def ttest_bindingsites(mol_file, encoding_file):
     """
     Test if ratar-encoding of a molecule (based on its structural file) produces the same result
     as the reference encoding for the same molecule.
