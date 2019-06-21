d4167b50c3d101d35805c7e7a194edce033febf1
Dominique Sydow <dominique.sydow@posteo.de>
2019-06-21 13:25:42 +0200

    Add unit test for Coordinates

--- ratar/tests/test_encoding_coordinates.py
+++ ratar/tests/test_encoding_coordinates.py
@@ -1,3 +1,57 @@
 """
 Unit and regression test for the Coordinates class in the ratar.encoding module of the ratar package.
 """
+
+import sys
+
+import pytest
+from pathlib import Path
+
+from ratar.auxiliary import MoleculeLoader
+from ratar.encoding import Representatives, Coordinates
+
+
+@pytest.mark.parametrize('filename, column_names, n_atoms, centroid', [
+    (
+        'AAK1_4wsq_altA_chainA_reduced.mol2',
+        'x y z'.split(),
+        {'ca': 8, 'pca': 34, 'pc': 29},
+        {'ca': [6.2681, 11.9717, 42.4514], 'pca': [5.6836, 12.9039, 43.9326], 'pc': [5.8840, 12.5871, 43.3804]}
+    )
+])
+def test_get_coordinates(filename, column_names, n_atoms, centroid):
+    """
+    Test if coordinates are correctly extracted from representatives of a molecule.
+
+    Parameters
+    ----------
+    filename : str
+        Name of molecule file.
+    column_names : list of str
+        List of molecule DataFrame columns.
+    n_atoms : dict of int
+        Number of atoms for each representatives type.
+    centroid : dict of list of float
+        3D coordinates of molecule centroid for each representatives type.
+    """
+
+    # Load molecule
+    molecule_path = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename
+    molecule_loader = MoleculeLoader()
+    molecule_loader.load_molecule(molecule_path)
+    molecule = molecule_loader.get_first_molecule()
+
+    # Set representatives
+    representatives = Representatives()
+    representatives.get_representatives(molecule)
+
+    # Set coordinates
+    coordinates = Coordinates()
+    coordinates.get_coordinates(representatives)
+
+    for key, value in coordinates.data.items():
+        assert all(value.columns == column_names)
+        assert value.shape[0] == n_atoms[key]
+        assert abs(value['x'].mean() - centroid[key][0]) < 0.0001
+        assert abs(value['y'].mean() - centroid[key][1]) < 0.0001
+        assert abs(value['z'].mean() - centroid[key][2]) < 0.0001
