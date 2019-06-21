98a0f7fc15d17a63d26c6d656ac45e458bad5c62
Dominique Sydow <dominique.sydow@posteo.de>
2019-06-21 14:30:17 +0200

    Add test_get_shapes (incomplete)

--- ratar/tests/test_encoding_shapes.py
+++ ratar/tests/test_encoding_shapes.py
@@ -1,3 +1,18 @@
 """
 Unit and regression test for the Shapes class in the ratar.encoding module of the ratar package.
 """
+
+import sys
+
+import pytest
+
+from ratar.auxiliary import MoleculeLoader
+
+
+@pytest.mark.parametrize('', [
+    (
+
+    )
+])
+def test_get_shapes():
+    pass
