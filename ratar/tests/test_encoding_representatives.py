74c4ca9cba0e7aa1e25c005239968da84c84ab19
Dominique Sydow <dominique.sydow@posteo.de>
2019-06-21 13:26:02 +0200

    Remove redundancy in Representatives unit test

--- ratar/tests/test_encoding_representatives.py
+++ ratar/tests/test_encoding_representatives.py
@@ -11,25 +11,40 @@ from ratar.auxiliary import MoleculeLoader
 from ratar.encoding import Representatives
 
 
-@pytest.mark.parametrize('filename, mol_n_atoms, ca_n_atoms', [
+@pytest.mark.parametrize('filename, column_names, n_atoms, centroid', [
     (
         'AAK1_4wsq_altA_chainA_reduced.mol2',
-        94,
-        8
+        {
+            'ca': 'atom_id atom_name res_id res_name subst_name x y z charge'.split(),
+            'pca': 'atom_id atom_name res_id res_name subst_name x y z charge pc_type pc_id pc_atom_id'.split(),
+            'pc': 'atom_id atom_name res_id res_name subst_name x y z charge pc_type pc_id pc_atom_id'.split()
+        },
+        {
+            'ca': 8,
+            'pca': 34,
+            'pc': 29
+        },
+        {
+            'ca': [6.2681, 11.9717, 42.4514],
+            'pca': [5.6836, 12.9039, 43.9326],
+            'pc': [5.8840, 12.5871, 43.3804]
+        }
     )
 ])
-def test_get_ca(filename, mol_n_atoms, ca_n_atoms):
+def test_get_representatives(filename, column_names, n_atoms, centroid):
     """
-    Test if pseudocenter atoms are correctly extracted from molecule.
+    Test if representatives are correctly extracted from representatives of a molecule.
 
     Parameters
     ----------
     filename : str
         Name of molecule file.
-    mol_n_atoms : int
-        Number of atoms in molecule.
-    ca_n_atoms : int
-        Number of Calpha atoms in molecule.
+    column_names : dict list of str
+        List of molecule DataFrame column names for each representatives type.
+    n_atoms : dict of int
+        Number of atoms for each representatives type.
+    centroid : dict of list of float
+        3D coordinates of molecule centroid for each representatives type.
     """
 
     # Load molecule
@@ -40,78 +55,11 @@ def test_get_ca(filename, mol_n_atoms, ca_n_atoms):
 
     # Set representatives
     representatives = Representatives()
-    representatives_pca = representatives._get_ca(molecule)
+    representatives.get_representatives(molecule)
 
-    assert molecule.shape == (mol_n_atoms, 9)
-    assert representatives_pca.shape == (ca_n_atoms, 9)
-
-
-@pytest.mark.parametrize('filename, mol_n_atoms, pca_n_atoms,', [
-    (
-        'AAK1_4wsq_altA_chainA_reduced.mol2',
-        94,
-        34
-    )
-])
-def test_get_pca(filename, mol_n_atoms, pca_n_atoms):
-    """
-    Test if pseudocenter atoms are correctly extracted from molecule.
-
-    Parameters
-    ----------
-    filename : str
-        Name of molecule file.
-    mol_n_atoms : int
-        Number of atoms in molecule.
-    pca_n_atoms : int
-        Number of pseudocenter atoms in molecule.
-    """
-
-    # Load molecule
-    molecule_path = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename
-    molecule_loader = MoleculeLoader()
-    molecule_loader.load_molecule(molecule_path)
-    molecule = molecule_loader.pmols[0].df
-
-    # Set representatives
-    representatives = Representatives()
-    representatives_pca = representatives._get_pca(molecule)
-
-    assert molecule.shape == (mol_n_atoms, 9)
-    assert representatives_pca.shape == (pca_n_atoms, 12)
-
-
-@pytest.mark.parametrize('filename, mol_n_atoms, pc_n_atoms', [
-    (
-        'AAK1_4wsq_altA_chainA_reduced.mol2',
-        94,
-        29
-    )
-])
-def test_get_pc(filename, mol_n_atoms, pc_n_atoms):
-    """
-    Test if pseudocenter atoms are correctly extracted from molecule.
-
-    Parameters
-    ----------
-    filename : str
-        Name of molecule file.
-    mol_n_atoms : int
-        Number of atoms in molecule.
-    pc_n_atoms : int
-        Number of pseudocenters in molecule.
-    """
-
-    # Load molecule
-    molecule_path = Path(sys.path[0]) / 'ratar' / 'tests' / 'data' / filename
-    molecule_loader = MoleculeLoader()
-    molecule_loader.load_molecule(molecule_path)
-    molecule = molecule_loader.pmols[0].df
-
-    # Set representatives
-    representatives = Representatives()
-    representatives_pc = representatives._get_pc(molecule)
-
-    assert molecule.shape == (mol_n_atoms, 9)
-    assert representatives_pc.shape == (pc_n_atoms, 12)
-
+    for key, value in representatives.data.items():
+        assert all(value.columns == column_names[key])
+        assert value.shape[0] == n_atoms[key]
+        assert abs(value['x'].mean() - centroid[key][0]) < 0.0001
+        assert abs(value['y'].mean() - centroid[key][1]) < 0.0001
+        assert abs(value['z'].mean() - centroid[key][2]) < 0.0001
