021db4a6ea275926b50bc11bb3b12ad7f013c536
dominiquesydow <dominique.sydow@posteo.de>
2021-12-20 20:08:02 +0100

    Turn BindingSite from_molecule classmethod back to method

--- ratar/tests/test_encoding_bindingsite.py
+++ ratar/tests/test_encoding_bindingsite.py
@@ -9,9 +9,9 @@ import pytest
 from ratar.encoding import BindingSite
 
 
-@pytest.mark.parametrize('mol_file1, mol_file2', [
-    ('AAK1_4wsq_altA_chainA.mol2', 'AAK1_4wsq_altA_chainB.mol2')
-])
+@pytest.mark.parametrize(
+    "mol_file1, mol_file2", [("AAK1_4wsq_altA_chainA.mol2", "AAK1_4wsq_altA_chainB.mol2")]
+)
 def test_bindingsites_eq(mol_file1, mol_file2):
     """
     Test __eq__ function for BindingSite class.
@@ -24,12 +24,16 @@ def test_bindingsites_eq(mol_file1, mol_file2):
         Name of file containing the structure for molecule B.
     """
 
-    molecule_path1 = Path(__name__).parent / 'ratar' / 'tests' / 'data' / mol_file1
-    molecule_path2 = Path(__name__).parent / 'ratar' / 'tests' / 'data' / mol_file2
+    molecule_path1 = Path(__name__).parent / "ratar" / "tests" / "data" / mol_file1
+    molecule_path2 = Path(__name__).parent / "ratar" / "tests" / "data" / mol_file2
 
-    bindingsite1 = BindingSite.from_file(molecule_path1)
-    bindingsite2 = BindingSite.from_file(molecule_path1)
-    bindingsite3 = BindingSite.from_file(molecule_path2)
+    bindingsite1 = BindingSite()
+    bindingsite2 = BindingSite()
+    bindingsite3 = BindingSite()
 
-    assert (bindingsite1 == bindingsite2)
+    bindingsite1.from_file(molecule_path1)
+    bindingsite2.from_file(molecule_path1)
+    bindingsite3.from_file(molecule_path2)
+
+    assert bindingsite1 == bindingsite2
     assert not (bindingsite1 == bindingsite3)
