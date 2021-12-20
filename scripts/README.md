d8ee6ca5982a9ce18957ad39550a7a997aa67924
dominiquesydow <dominique.sydow@posteo.de>
2021-12-20 23:17:15 +0100

    Revise scripts; update README with benchmarking datasets

--- scripts/README.md
+++ scripts/README.md
@@ -1,1 +1,7 @@
 This `scripts` folder contains script files using the `ratar` package.
+
+- `ratar.sh`: Try out CLI with different datasets
+- `benchmarking.py`: Calculate the similarity values for binding site pairs described in different benchmarking datasets.
+  - FuzCav dataset: Upon request from authors of https://doi.org/10.1021/ci900349y (saved in Dominique's backup `dominiquesydow/Documents/Projects/Ratar/Data/benchmarking/fuzcav`)
+  - ProSPECCTs dataset: https://doi.org/10.1371/journal.pcbi.1006483
+  - TOUGH_M1 dataset: https://doi.org/10.17605/OSF.IO/6NGBS
--- scripts/ratar.sh
+++ scripts/ratar.sh
@@ -13,7 +13,7 @@ output_dir="/home/dominique/Documents/projects/ratar-data/results"
 ########################################################################################
 
 # Data set scPDB-EFGR
-#ratar -i ${data_dir}"/scpdb/egfr_20190128/scPDB_20190128_egfr.mol2" -o ${output_dir}"/scpdb/egfr_20190128/"
+ratar -i ${data_dir}"/scpdb/egfr_20190128/scPDB_20190128_egfr.mol2" -o ${output_dir}"/scpdb/egfr_20190128/"
 
 
 ########################################################################################
@@ -21,24 +21,26 @@ output_dir="/home/dominique/Documents/projects/ratar-data/results"
 ########################################################################################
 
 # Full scPDB
-#ratar -i ${data_dir}"/scpdb/full_20180807/*/site.mol2" -o ${output_dir}
+ratar -i ${data_dir}"/scpdb/full_20180807/*/site.mol2" -o ${output_dir}
 
 # Subset of scPDB
-#ratar -i ${data_dir}"/scpdb/test_20180807/*/site.mol2" -o ${output_dir}"/scpdb/test_20180807/"
+ratar -i ${data_dir}"/scpdb/test_20180807/*/site.mol2" -o ${output_dir}"/scpdb/test_20180807/"
 
 # Benchmarking dataset - FuzCav
-#ratar -i ${data_dir}"/benchmarking/fuzcav/sim_dis_pairs/structures/*/site_CA_Met.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
+ratar -i ${data_dir}"/benchmarking/fuzcav/sim_dis_pairs/structures/*/site_CA_Met.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
 
 # Benchmarking dataset - TOUGH-M1
-#ratar -i ${data_dir}"/benchmarking/TOUGH-M1/structures/*/site_CA_Met.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
+ratar -i ${data_dir}"/benchmarking/TOUGH-M1/structures/*/site_CA_Met.mol2" -o ${output_dir}"/benchmarking/fuzcav/sim_dis_pairs/"
 
 # KLIFS test
 ratar -i ${data_dir}"/klifs/test_20190506/structures/HUMAN/*/*/pocket.mol2" -o ${output_dir}"/klifs/test_20190506/HUMAN/"
 
 # KLIFS full
-#ratar -i ${data_dir}"/klifs/klifs_20190506/structures/HUMAN/*/*/pocket.mol2" -o ${output_dir}"/klifs/klifs_20190506/HUMAN/"
+ratar -i ${data_dir}"/klifs/klifs_20190506/structures/HUMAN/*/*/pocket.mol2" -o ${output_dir}"/klifs/klifs_20190506/HUMAN/"
 
 
 ########################################################################################
 # Multiple pdb files
 ########################################################################################
+
+# TBA
