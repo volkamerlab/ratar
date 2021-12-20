6cba89a1520f2f94fbff34681dd668babba202ca
dominiquesydow <dominique.sydow@posteo.de>
2021-12-20 22:28:39 +0100

    Add CLI ratar encode/compare subcommands (compare not implemented)

--- ratar/cli.py
+++ ratar/cli.py
@@ -12,12 +12,14 @@ import datetime
 import logging
 import logging.config
 from pathlib import Path
+import subprocess
+import platform
 
 from .auxiliary import create_directory
 from .encoding import process_encoding
 
 
-def parse_arguments():
+def main():
     """
     Parse the arguments given when calling this script.
 
@@ -29,46 +31,90 @@ def parse_arguments():
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument(
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    # Subcommand encode
+    encode_subparser = subparsers.add_parser("encode")
+    encode_subparser.add_argument(
         "-i", "--input_mol_path", help="Path to molecule structure file(s).", required=True
     )
-    parser.add_argument("-o", "--output_dir", help="Path to output directory.", required=True)
+    encode_subparser.add_argument(
+        "-o", "--output_dir", help="Path to output directory.", required=True
+    )
+    encode_subparser.set_defaults(func=main_encode)
+
+    # Subcommand compare
+    compare_subparser = subparsers.add_parser("compare")
+    compare_subparser.set_defaults(func=main_compare)
 
     # Set as variables
     args = parser.parse_args()
+    try:
+        args.func(args)
+    except AttributeError:
+        # Run help if no arguments were given
+        subprocess.run(["ratar", "-h"])
 
-    return args
 
-
-def main():
+def configure_logger(filename=None, level=logging.INFO):
     """
-    Main ratar function to process one or multiple binding sites.
+    Configure logging.
+    Parameters
+    ----------
+    filename : str or None
+        Path to log file.
+    level : int
+        Logging level for ratar package (default: INFO).
+    """
+
+    # Create custom logger
+    logger = logging.getLogger(__name__)
+    # Set logger levels
+    logger.setLevel(level)
+    # Get formatter
+    formatter = logging.Formatter(logging.BASIC_FORMAT)
+    # Set a stream and a file handler
+    # Create handlers
+    s_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler(filename=Path(filename) / "ratar.log")
+    # Set formatting for these handlers
+    s_handler.setFormatter(formatter)
+    # Add both handlers to both loggers
+    logger.addHandler(s_handler)
+    logger.addHandler(f_handler)
+
+    # Set up file handler if
+    # - log file is given
+    # - we are not under Windows, since logging and multiprocessing do not work here
+    #   see more details here: https://github.com/volkamerlab/kissim/pull/49
+    if filename and platform.system() != "Windows":
+        filename = Path(filename)
+        f_handler = logging.FileHandler(filename.parent / f"{filename.stem}.log", mode="w")
+        f_handler.setFormatter(formatter)
+        logger.addHandler(f_handler)
+
+    return logger
+
+
+def main_encode(args):
+    """
+    Encode structures.
+
+    Parameters
+    ----------
+    args : argsparse.Namespace
+        CLI arguments.
     """
 
     # Get start time of script
     encoding_start = datetime.datetime.now()
 
-    # Parse arguments
-    args = parse_arguments()
-
     # Create output folder
     create_directory(args.output_dir)
 
-    # Create custom logger
-    logging.config.fileConfig(
-        "logging.conf", defaults={"logfilename": str(Path(args.output_dir) / "ratar.log")}
-    )
-
-    # Create handlers
-    c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(filename=Path(args.output_dir) / "ratar.log")
-
-    # Get handler
-    logger = logging.getLogger(__name__)
-
-    # Add handler to logger
-    logger.addHandler(c_handler)
-    logger.addHandler(f_handler)
+    logger = configure_logger(args.output_dir)
 
     # Log IO
     logger.info("IO", extra={"molecule_id": "all"})
@@ -88,5 +134,14 @@ def main():
     logger.info(f"Encoding step: {encoding_runtime}", extra={"molecule_id": "all"})
 
 
-if __name__ == "__main__":
-    main()
+def main_compare(args):
+    """
+    Compare structures.
+
+    Parameters
+    ----------
+    args : argsparse.Namespace
+        CLI arguments.
+    """
+
+    raise NotImplementedError(f"Calculating similarities from the CLI is not implemented, yet.")
